ecb0d73f5ca090e1d5d0c4169d982ffb82086892
Tiffany Tang <tiffanytangt@users.noreply.github.com>
2016-04-18 02:51:47 -0700

    add authentication on login page via back-end API
    -does not redirect, response is printed to console

---
+++ assets/app/controllers/AuthController.js
@@ -0,0 +1,35 @@
+sbapp.controller('AuthController', [
+  '$http',
+  AuthController
+]);
+
+function AuthController($http) {
+  var vm = this;
+
+  vm.user = 'username';
+  vm.password = 'password';
+  vm.authenticate = authenticate;
+
+  function authenticate(usr, pwd) {
+    var data = {
+      "identifier": usr,
+      "password": pwd
+    };
+    $http.post('/auth/local', data, null).then(authSuccess, authError);
+  }
+
+  function authSuccess(response) {
+    vm.data = response.data;
+    vm.status = response.status;
+    console.log(response.data);
+  }
+
+  function authError(response) {
+    vm.data = response.data || "Request Failed";
+    vm.status = response.status;
+    console.log(response.data);
+
+  }
+
+
+}
