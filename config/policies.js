72c9839870329c5eabb07d33d8756549032b81f7
Tiffany Tang <tiffanytangt@users.noreply.github.com>
2016-04-18 02:45:28 -0700

    installed authentication/authorization modules

--- config/policies.js
+++ config/policies.js
@@ -27,6 +27,21 @@ module.exports.policies = {
   ***************************************************************************/
 
   // '*': true,
+  '*': [
+    'basicAuth',
+    'passport',
+    'sessionAuth',
+    'ModelPolicy',
+    'AuditPolicy',
+    'OwnerPolicy',
+    'PermissionPolicy',
+    'RolePolicy',
+    'CriteriaPolicy'
+  ],
+
+  AuthController: {
+    '*': ['passport']
+  },
 
   /***************************************************************************
   *                                                                          *
