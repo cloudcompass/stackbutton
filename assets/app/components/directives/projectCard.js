ad35d6c75cf7156202a0b99a0abb20ea2c9231c3
Tiffany Tang <tiffanytangt@users.noreply.github.com>
2016-04-21 02:24:01 -0700

    Created project-card directive for directory view

---
+++ assets/app/components/directives/projectCard.js
@@ -0,0 +1,28 @@
+sbapp.directive('projectCard', function () {
+  return {
+    restrict: 'E',
+    replace: true,
+    transclude: true,
+    scope: {title: '@', template: '@', options: '@'},
+    template: '' +
+    '<section layout-margin class="md-whiteframe-z1 panel-widget">' +
+    '  <md-toolbar md-theme="custom" class="md-hue-1 panel-widget-toolbar fixed-height-widget fixed-width">' +
+    '    <div class="md-toolbar-tools">' +
+    '      <h4 class="panel-widget-tittle">{{title}}</h4>' +
+    '      <span flex></span>' +
+    '      <md-button ng-show="options" ng-click="$showOptions = !$showOptions" class="md-icon-button" aria-label="Show options">' +
+    '        <i class="material-icons">more_vert</i>' +
+    '      </md-button>' +
+    '    </div>' +
+    '  </md-toolbar>' +
+    '  <div ng-include="template"/>' +
+    '</section>',
+    compile: function (element, attrs, linker) {
+      return function (scope, element) {
+        linker(scope, function (clone) {
+          element.append(clone);
+        });
+      };
+    }
+  };
+});
