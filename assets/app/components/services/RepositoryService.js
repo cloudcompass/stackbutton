ca1f9c6080f3901d6ab9de050fe2f911293f5dce
pcridge <p.cridge03@gmail.com>
2016-04-24 14:38:48 -0700

    Linked Back-end to Front-end, Repository widget

---
+++ assets/app/components/services/RepositoryService.js
@@ -0,0 +1,18 @@
+sbapp.service('repositoryService', ['$q', repositoryService]);
+
+function repositoryService($q) {
+  var commits = [
+    {
+      description: 'Linked Controller to View',
+      author: 'Paul Cridge',
+      date: '2016-04-24',
+      files: '1'
+    }
+  ];
+
+  return {
+    loadAllItems: function () {
+      return $q.when(commits);
+    }
+  };
+}
