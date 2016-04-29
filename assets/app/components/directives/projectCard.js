08f70c5535b381814eaf32675af58aca28f66481
pcridge <p.cridge03@gmail.com>
2016-04-29 15:36:25 -0700

    Added styling projectCard Directive

--- assets/app/components/directives/projectCard.js
+++ assets/app/components/directives/projectCard.js
@@ -6,22 +6,26 @@ sbapp.directive('projectCard', function () {
     scope: {title: '@',description: '@', options: '@'},
     template: '' +
     '<section layout-margin class="md-whiteframe-z1 panel-widget fixed-height-widget fixed-width">' +
-    '  <md-toolbar md-theme="custom" class="md-hue-1 panel-widget-toolbar">' +
-    '    <div class="md-toolbar-tools" layout="row" layout-align="space-between start">' +
-    '      <h4 style="color: teal" class="panel-widget-tittle">{{title}}</h4>' +
+
+    '    <div class="md-title" layout="row" layout-align="space-between start">' +
+    '      <md-button ng-show="!hideOptions" ui-sref="home.dashboard">' +
+    '       <span style="color: #2b81af; font-size: large" class="panel-widget-tittle">{{title}}</span>' +
+    '      </md-button>' +
+    '      <span class="panel-widget-tittle" layout-padding ng-show="hideOptions">Options:</span>' +
     '      <md-button ng-click="hideOptions=!hideOptions" class="md-icon-button" aria-label="Show options">' +
     '        <i class="material-icons" ng-show="!hideOptions">flip_to_back</i>' +
     '        <i class="material-icons" ng-show="hideOptions">flip_to_front</i>' +
     '      </md-button>' +
     '    </div>' +
-    '  </md-toolbar>' +
+    ' <md-divider ng-show="!hideOptions"></md-divider>' +
     '  <div ng-show="!hideOptions">' +
     '   {{description}}' +
     '  </div>' +
-    '  <div ng-show="hideOptions">' +
+    '  <div ng-show="hideOptions" layout="column" layout-align="start center">' +
     '   <md-button class="md-raised" ui-sref="home.plugin" ng-click="$mdOpenMenu($event)">Edit Project</md-button>' +
-    '   <md-button class="md-raised md-warn" ng-click="vm.showDialog()">Delete Project</md-button>' +
+    '   <md-button class="md-raised md-warn" ng-click="showDeleteDialog()">Delete Project</md-button>' +
     '  </div>' +
+    ' <md-divider ng-show="!hideOptions"></md-divider>' +
     '</section>',
     compile: function (element, attrs, linker) {
       return function (scope, element) {
