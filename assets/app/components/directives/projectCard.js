7eee442043fcf28d38af4fed0eb5590e4de5f305
Brendan Davidson <science_davidson@hotmail.com>
2016-04-28 20:40:08 -0700

    Removed template from Project Card

--- assets/app/components/directives/projectCard.js
+++ assets/app/components/directives/projectCard.js
@@ -15,7 +15,7 @@ sbapp.directive('projectCard', function () {
     '      </md-button>' +
     '    </div>' +
     '  </md-toolbar>' +
-    '  <div ng-show="!hideOptions" ng-include="template"/>' +
+    '  <div ng-show="!hideOptions"/>' +
     '  <div ng-show="hideOptions">'+
     '   <md-button class="md-raised" ui-sref="home.plugin" ng-click="$mdOpenMenu($event)">Edit Project</md-button>' +
     '   <md-button class="md-raised md-warn" ng-click="vm.showDialog()">Delete Project</md-button>' +
