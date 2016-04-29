27521edeee60119f3ddc3735081242b9ac06e9b6
Brendan Davidson <science_davidson@hotmail.com>
2016-04-29 14:45:57 -0700

    Removed redirect and replaced with error

--- assets/app/controllers/CreateController.js
+++ assets/app/controllers/CreateController.js
@@ -11,26 +11,23 @@ function CreateController($scope, ProjectService) {
 
   console.log('Loaded Create Controller');
 
-  //If the user has not logged in, redirect them to the login
-  // (IF USER == NULL)
-  if($scope.currentUser == null){
-    console.log('USER IS NULL');
-    window.location.replace('/#/login');
-  }
 
   vm.addProject = function (name, description) {
     console.log('Add Project called');
-
-    ProjectService.addProject(name, description, $scope.currentUser.id)
-      .then(
-        function (project) { vm.error = ''; },
-        function (res) {
-          vm.error = 'Couldn\'t create project';
-          for (item in res.data.invalidAttributes) {
-            vm.error += res.data.invalidAttributes[item][0].message + '\r\n';
+    if($scope.currentUser == null){
+      console.log('USER IS NULL');
+    }else{
+      ProjectService.addProject(name, description, $scope.currentUser.id)
+        .then(
+          function (project) { vm.error = ''; },
+          function (res) {
+            vm.error = 'Couldn\'t create project';
+            for (item in res.data.invalidAttributes) {
+              vm.error += res.data.invalidAttributes[item][0].message + '\r\n';
+            }
           }
-        }
-      );
+        );
+    }
   };
 
   // vm.register = function (username, email, password, passwordVerify) {
