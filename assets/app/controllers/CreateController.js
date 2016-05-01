db0a4dbc8a99c5caabe05964a970048be23c526f
Tiffany Tang <tiffanytangt@users.noreply.github.com>
2016-05-01 04:38:36 -0700

    added owner-specific project loading

--- assets/app/controllers/CreateController.js
+++ assets/app/controllers/CreateController.js
@@ -1,47 +1,35 @@
 sbapp.controller('CreateController', [
+  '$state',
   '$scope',
   'ProjectService',
   CreateController
 ]);
 
-function CreateController($scope, ProjectService) {
+function CreateController($state, $scope, ProjectService) {
   // Expose variables and functions to the view:
   var vm = this;
   vm.error = null;
 
-  console.log('Loaded Create Controller');
-
-
   vm.addProject = function (name, description) {
-    console.log('Add Project called');
     if($scope.currentUser == null){
-      console.log('USER IS NULL');
-    }else{
+      console.log('addProject(): null user. Aborting.');
+    } else {
       var newProj = {
         name: name,
         description: description,
         ownerId: $scope.currentUser.id
       };
-      console.log('addProject() adding:', newProj);
       ProjectService.save(newProj,
         function (project, headers) {
           //success callback
           console.log('addProject() success:', project);
+          $state.go('home.projects');
         },
         function (resp) {
           //error callback
           console.log('addProject() error:', resp);
         }
       );
-      // .then(
-      //   function (project) { vm.error = ''; },
-      //   function (res) {
-      //     vm.error = 'Couldn\'t create project';
-      //     for (item in res.data.invalidAttributes) {
-      //       vm.error += res.data.invalidAttributes[item][0].message + '\r\n';
-      //     }
-      //   }
-      // );
     }
   };
 
--- assets/app/controllers/projectController.js
+++ assets/app/controllers/projectController.js
@@ -1,13 +1,11 @@
 sbapp.controller('ProjectController', [
+  '$scope',
   'ProjectService',
   '$mdDialog',
   ProjectController
 ]);
 
-
-
-
-function ProjectController(ProjectService, $mdDialog) {
+function ProjectController($scope, ProjectService, $mdDialog) {
   var vm = this;
 
   console.log("projcontrollerloaded");
@@ -19,27 +17,29 @@ function ProjectController(ProjectService, $mdDialog) {
       escapeToClose: true,
       template: '<delete-project></delete-project>'
     });
-  }
+  };
+
+  vm.projects = ProjectService.query({ownerId: $scope.currentUser.id});
 
 
-  vm.projects = [
-    {
-      name: 'Project 1',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tincidunt varius nulla quis ullamcorper.'
-    },
-    {
-      name: 'Project 2',
-      description: 'Vestibulum lacinia volutpat sapien, et faucibus lectus blandit ac.'
-    },
-    {
-      name: 'Project 3',
-      description: 'Nunc sollicitudin magna vitae ex porta varius.'
-    },
-    {
-      name: 'Project 4',
-      description: 'Fusce magna dui, pellentesque tincidunt posuere eu, varius at ligula.'
-    }
-  ];
+  // vm.projects = [
+  //   {
+  //     name: 'Project 1',
+  //     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tincidunt varius nulla quis ullamcorper.'
+  //   },
+  //   {
+  //     name: 'Project 2',
+  //     description: 'Vestibulum lacinia volutpat sapien, et faucibus lectus blandit ac.'
+  //   },
+  //   {
+  //     name: 'Project 3',
+  //     description: 'Nunc sollicitudin magna vitae ex porta varius.'
+  //   },
+  //   {
+  //     name: 'Project 4',
+  //     description: 'Fusce magna dui, pellentesque tincidunt posuere eu, varius at ligula.'
+  //   }
+  // ];
 
 }
 
