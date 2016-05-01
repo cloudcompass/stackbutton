3fa13e3d5440de4a90eeefd6d023c37786ae32eb
Brendan Davidson <science_davidson@hotmail.com>
2016-05-01 16:44:14 -0700

    Added card view to "add a tool"

--- assets/app/controllers/ToolController.js
+++ assets/app/controllers/ToolController.js
@@ -4,18 +4,34 @@ sbapp.controller('ToolController', [
 
 function ToolController() {
   var vm = this;
-  vm.incCount = incCount;
-  vm.decCount = decCount;
+  vm.back = back;
+  vm.next = next;
+  vm.gitSelect = gitSelect;
+  vm.bitSelect = bitSelect;
 
   //Used for page back/next and div displays on addATool.html
   vm.count = 0;
 
-  function incCount(){
-    this.count += 1;
+  //Using a numeric system to hide and show pages of this setup
+  // 0 = Main page
+  // 1 = Github setup
+  // 2 = other. - Bitbucket setup is not implemented
+
+  function back(){
+    this.count = 0;
   }
 
-  function decCount(){
+  function next(){
     this.count -= 1;
   }
 
+  function gitSelect(){
+    console.log("github selected");
+    this.count = 1;
+  }
+
+  function bitSelect(){
+    console.log("Bitbucket selected");
+    this.count = 2;
+  }
 }
