19aed4f6965fa0049c5815d71cbb5d61a5860c9b
Brendan Davidson <science_davidson@hotmail.com>
2016-05-23 00:49:50 -0700

    Migrated delete dialog from projectController to EditProject

--- assets/app/controllers/EditProject.js
+++ assets/app/controllers/EditProject.js
@@ -1,13 +1,69 @@
 sbapp.controller('EditProject', [
   '$scope',
   '$state',
+  '$mdDialog',
   'ProjectService',
   EditProject
 ]);
 
-function EditProject($scope, $state, ProjectService) {
+function EditProject($scope, $state, $mdDialog, ProjectService) {
   var vm = this;
   vm.updateProject = updateProject;
+
+  //vm.showDeleteDialog = showDeleteDialog;
+
+  //DELETE PROJECT DIALOG
+  vm.showDeleteDialog = function (project) {
+    //Grabbing current project to delete
+    var currentProject = $scope.currentProject;
+
+    console.log("Delete Function Called");
+    $mdDialog.show({
+      clickOutsideToClose: true,
+      escapeToClose: true,
+      scope: $scope,
+      preserveScope: true,
+      template: '' +
+      '<md-card style="max-width: 350px;">' +
+        '<div>' +
+          '<p class="md-headline">Would you like to permanently DELETE ' + currentProject.name + '?</p>' +
+        '</div>' +
+        '<div>' +
+          '<p class="md-headline">Please type the name of the project to confirm deletion.</p>' +
+          '<md-input-container>' +
+            '<label>Project Name</label>' +
+            '<input type="text" ng-model="confirmBox' + currentProject.id + '" required>' +
+          '</md-input-container>' +
+        '</div>' +
+        '<md-button class="md-primary" ng-if="confirmBox' + currentProject.id + '==\'' + currentProject.name + '\'" ng-click="confirm(' + currentProject.id + ')">DELETE PROJECT</md-button>' +
+        '<md-button class="md-warn" ui-sref="home.projects" ng-click="cancelDialog()">Cancel</md-button>' +
+      '</md-card>',
+      controller: function DialogController($scope, $mdDialog) {
+        $scope.confirm = function (id) {
+          projectResource = ProjectService.project.remove({projid: id});
+          console.log("deleted", projectResource);
+          $mdDialog.hide();
+
+        };
+        $scope.cancelDialog = function () {
+          $mdDialog.cancel();
+          // clear text input:
+          eval('$scope.confirmBox' + currentProject.id + '=null');
+        }
+      }
+    }).then(
+      // dialog resolved handler
+      function () {
+        //Redundant go code to fix issue with page not reloading properly after project deletion
+        $state.go('home.projects');
+        $state.reload();
+        $state.go('home.projects');
+      },
+      // dialog cancelled handler
+      function () {
+        console.log('dialog cancelled');
+      });
+  };
 
   //UPDATE PROJECT DESCRIPTION
   function updateProject(newName, newDescription) {
