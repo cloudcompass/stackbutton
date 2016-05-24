aaa07d917fccc35155de3f3b35f471c44dc5d05e
Tiffany Tang <tiffanytangt@users.noreply.github.com>
2016-05-24 16:01:11 -0700

    fix: added unversioned backend files for issues widget and activity feed

---
+++ api/controllers/EventController.js
@@ -0,0 +1,9 @@
+/**
+ * EventController
+ *
+ * @description :: Server-side logic for managing Events
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {};
+
