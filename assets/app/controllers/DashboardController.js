a82c1787dd5086112aee93b267a39076252c7906
Tiffany Tang <tiffanytangt@users.noreply.github.com>
2016-06-06 08:31:14 -0700

    added empty dashboard/projects message to views

--- assets/app/controllers/DashboardController.js
+++ assets/app/controllers/DashboardController.js
@@ -8,21 +8,29 @@ sbapp.controller('DashboardController', [
 function DashboardController($stateParams, $scope, ProjectService) {
   var vm = this;
   vm.widgets = [];
+  vm.loading = false;
   console.log($stateParams);
+  $scope.setCurrentProject(null);
+  loadWidgets();
 
-  ProjectService.dashboard.get({id: $stateParams.dashboardId, populate: ['widgets', 'project']},
-    function (dashboard, headers) {
-      vm.widgets = dashboard.widgets;
-      ProjectService.project.get({id: dashboard.project.id, populate: ['dashboards']},
-        function (project) {
-          $scope.setCurrentProject(project);
-        });
-    },
-    function (err) {
-      console.log('error:', err);
-    }
-  );
 
+  function loadWidgets() {
+    vm.loading = true;
+    ProjectService.dashboard.get({id: $stateParams.dashboardId, populate: ['widgets', 'project']},
+      function (dashboard, headers) {
+        vm.widgets = dashboard.widgets;
+        vm.loading = false;
+        ProjectService.project.get({id: dashboard.project.id, populate: ['dashboards']},
+          function (project) {
+            $scope.setCurrentProject(project);
+          });
+      },
+      function (err) {
+        console.log('error:', err);
+        vm.loading = false;
+      }
+    );
+  }
 
 }
 
