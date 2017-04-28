c38be26b21d83fd9b4c69a6167ddbd4ad9abef99
Curtis LayCraft <curtis.laycraft@zoho.com>
2017-04-28 09:31:11 -0700

    This is for a basic top nav component.

--- ng2/src/app/app.module.ts
+++ ng2/src/app/app.module.ts
@@ -2,14 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+
+import { TopNavComponent } from './navbar/top-nav.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    TopNavComponent,
   ],
   imports: [
     BrowserModule,
