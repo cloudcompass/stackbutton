a389129573421dca62f724e943bc5b39eaa2f595
HellaBored <leecombs_30@hotmail.com>
2017-04-28 13:34:14 -0700

    Revert "This is for a basic top nav component."
    This reverts commit c38be26b21d83fd9b4c69a6167ddbd4ad9abef99.

--- ng2/src/app/app.module.ts
+++ ng2/src/app/app.module.ts
@@ -2,17 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-
-import { TopNavComponent } from './navbar/top-nav.component';
-
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    TopNavComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
