05d454d2778c02d58d3745808815a7aea9fbc9d0
Curtis <curtis.laycraft@zoho.com>
2017-05-03 15:42:24 -0700

    Changed style to drop down instead of vertical nav.

--- ng2/src/app/vertical-navigation/vertical-navigation.component.ts
+++ ng2/src/app/vertical-navigation/vertical-navigation.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-vertical-navigation',
   templateUrl: './vertical-navigation.component.html',
-  styleUrls: ['./vertical-navigation.component.css']
+  // styleUrls: ['./vertical-navigation.component.css']
 })
 export class VerticalNavigationComponent implements OnInit {
 
