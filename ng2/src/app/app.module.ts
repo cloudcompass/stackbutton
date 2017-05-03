1e5100a9acff230e319e031355d7a81db01ecf33
Curtis LayCraft <curtis.laycraft@zoho.com>
2017-05-02 18:16:33 -0700

    Created base level demo patternfly dashboard.

--- ng2/src/app/app.module.ts
+++ ng2/src/app/app.module.ts
@@ -5,10 +5,18 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavVerticalComponent } from './nav-vertical/nav-vertical.component';
+import { NavHorizontalComponent } from './nav-horizontal/nav-horizontal.component';
+import { BlankStateComponent } from './blank-state/blank-state.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    NavVerticalComponent,
+    NavHorizontalComponent,
+    BlankStateComponent
   ],
   imports: [
     BrowserModule,
