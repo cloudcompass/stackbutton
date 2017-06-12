9af02690973dde0e6e969cc9aacc74f2f92bf731
HellaBored <leecombs_30@hotmail.com>
2017-06-12 14:16:45 -0700

    DataSource model and sample data created

---
+++ ng2/src/app/_models/dataSourceModel.ts
@@ -0,0 +1,21 @@
+export class DataSourceModel {
+  sourceID: string; // Unique dataSource key
+  projectName: string;
+  projectID: string;
+  teamName: string;
+  teamMembers: {
+    name: string;
+    role: string;
+    uuid: string;
+  }[];
+  service: {
+    type: string; // Github, OpenShift, etc.
+    apikey: string;
+    misc: any[]; // ?
+  };
+  serviceID: string; // Database Key
+  metadata: {
+    location: string;
+    creationTimestamp: string;
+  };
+}
